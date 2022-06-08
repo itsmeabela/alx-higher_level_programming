f7098d9fa153dfc861b681d60b503f20883b656c
itsmeabela <abelrobel7@gmail.com>
2022-06-08 22:53:15 +0300

    debuged

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,12 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    maxval = 0
-    maxkey = None
+    com = list(a_dictionary.keys())[0]
+    large = a_dictionary[com]
     for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxval = k
-    return maxkey
+        if v > large:
+            large = v
+            com = k
+    return com
